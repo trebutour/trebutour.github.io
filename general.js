4716d6af1ee660743a45f33b0ad3c990d4b3b2c6
JMIDEV <trebutour2@gmail.com>
2022-04-02 18:28:01 +0200

    cuenta atrás actualización 14

--- general.js
+++ general.js
@@ -1,4 +1,39 @@
-var pageblock = true;
+var testmode = false;
+var testvalue = false;
+var pageblock;
+
+var pageopendate = new Date();
+var releasedate = new Date(2022, 03, 05);
+
+var hoursleft = 23 - pageopendate.getHours();
+var minsleft = 59 - pageopendate.getMinutes();
+var secsleft = 59 - pageopendate.getSeconds();
+
+var oneDay = 24 * 60 * 60 * 1000;
+var daysleftpure = -((pageopendate - releasedate) / oneDay);
+var daysleft = Math.floor(daysleftpure);
+
+if(!testmode){
+
+    if (daysleft < 0){
+
+        pageblock = false;
+
+    }
+
+    else{
+
+        pageblock = true;
+
+    }
+
+}
+
+else{
+
+    pageblock = testvalue;
+
+}
 
 if(pageblock){
 
@@ -20,16 +55,6 @@ else{
 
 //COUNTER
 
-var pageopendate = new Date();
-var releasedate = new Date(2022, 03, 05);
-
-var hoursleft = 23 - pageopendate.getHours();
-var minsleft = 59 - pageopendate.getMinutes();
-var secsleft = 59 - pageopendate.getSeconds();
-
-var oneDay = 24 * 60 * 60 * 1000;
-var daysleft = Math.floor(Math.abs((pageopendate - releasedate) / oneDay));
-
 function return_zeroed(value, digitnmb){
 
     return ('000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000' + value).slice(-digitnmb)
@@ -38,15 +63,26 @@ function return_zeroed(value, digitnmb){
 
 function updatetext(){
 
-    if(pageblock){
+    if(pageblock && daysleft >= 0){
     
         document.getElementById("TIMETEXT").innerHTML = "Quedan: " + return_zeroed(daysleft, 2) + " días, " + return_zeroed(hoursleft, 2) + ":" + return_zeroed(minsleft, 2) + ":" + return_zeroed(secsleft, 2) + " para la <span style='font-size: 7.5vmin;'>NUEVA WEB</span>.";
         var currentdate = new Date();
-        var percuntilopen = ((Math.abs((currentdate - releasedate) / oneDay))/6)*100;
+        var percuntilopen = ((-((currentdate - releasedate) / oneDay))/6)*100;
         $("#progresscontent").css("width", 100-percuntilopen + "%");
-        document.getElementById("PROGRESSTEXT").innerHTML = "Proximamente, la nueva web de Trebutour... La espera va por el " + Math.floor(100-percuntilopen) + "%.";
+        document.getElementById("PROGRESSTEXT").innerHTML = "Próximamente, la nueva web de Trebutour... La espera va por el " + Math.floor(100-percuntilopen) + "%.";
+        
 
     }
+
+    else if(pageblock && daysleft < 0){
+
+        document.getElementById("TIMETEXT").innerHTML = "REINICIA/REABRE LA PÁGINA";
+        $("#TIMETEXT").css("transform", "translateX(-50%) translateY(-50%)");
+        $("#progressbar").css("display", "none");
+
+    }
+
+    console.log(daysleft);
 
 }
 
